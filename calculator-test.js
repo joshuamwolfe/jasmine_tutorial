10415e2181d4a7a6251aa4a550261952602f987f
Joshua Wolfe <joshuamwolfe@gmail>
2020-06-23 12:36:42 -0400

    jasmine testing

--- calculator-test.js
+++ calculator-test.js
@@ -1,10 +1,15 @@
-it('should calculate the monthly rate correctly', function () {
-  expect(calculateMonthlyPayment().toEqual(224));
+it("matches objects with the expect key/value pairs", function() {
+  expect(setupIntialValues).toEqual(jasmine.objectContaining({
+    amount: 10000
+  }));
+  expect(setupIntialValues).toEqual(jasmine.objectContaining({
+    years: 10
+  }));
+  expect(setupIntialValues).toEqual(jasmine.objectContaining({
+    rate: 4.5
+  }));
 });
-
-
-it("should return a result with 2 decimal places", function() {
-  // ..
-});
-
-/// etc
+//dev ref list
+  //jasmine object help
+  //https://stackoverflow.com/questions/31528200/jasmine-test-for-object-properties
+  
