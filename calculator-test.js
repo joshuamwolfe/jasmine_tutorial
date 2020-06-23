4c5088d4c502374bbf590dd285c3fc7da4fc0ce9
Joshua Wolfe <joshuamwolfe@gmail>
2020-06-23 11:14:50 -0400

    intial commit

---
+++ calculator-test.js
@@ -0,0 +1,10 @@
+it('should calculate the monthly rate correctly', function () {
+  expect(calculateMonthlyPayment().toEqual(224));
+});
+
+
+it("should return a result with 2 decimal places", function() {
+  // ..
+});
+
+/// etc
